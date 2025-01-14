1bdb6272d64643d9687948adecbed1a26297987e
J16N <jishan2001bhattacharya@gmail.com>
2025-01-14 19:56:21 +0530

    Add corsica

--- backend/mix.exs
+++ backend/mix.exs
@@ -39,7 +39,8 @@ defmodule Backend.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:httpoison, "~> 2.2.1"}
+      {:httpoison, "~> 2.2.1"},
+      {:corsica, "~> 2.1.3"}
     ]
   end
 
