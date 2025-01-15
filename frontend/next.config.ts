fdcc40693c011735305c2b47b78fa186c4e0c2a4
J16N <jishan2001bhattacharya@gmail.com>
2025-01-15 22:14:32 +0530

    Load from .env file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import { config } from "dotenv";
+
+const { parsed: myEnv } = config();
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config, { isServer, webpack }) {
+    if (!isServer) {
+      config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
